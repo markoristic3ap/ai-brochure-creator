03ceee4863a1363eb9b4a4f5aeda41af7cdcba15
Marko Ristic <marko@3ap.ch>
2025-02-08 15:24:34 +0100

    parsing the links from privided website

---
+++ main.py
@@ -0,0 +1,4 @@
+from website import Website
+
+website = Website('http://creativewin.net')
+print(website.get_links())
---
+++ website.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+class Website:
+    def __init__(self, url):
+        self.url = url
+        response = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+        links = [link.get('href') for link in soup.find_all('a')]
+        self.links = [link for link in links if link]
+
+    def get_contents(self):
+        return f"Webpage Title: {self.title}\nWebpage Contents: {self.text}\n\n"
+
+    def get_links(self):
+        return f"Website Links:\n{self.links}\n\n"
