36351fee85f6cd355436597c7c9ec481aa9a57bb
Marko Ristic <marko@3ap.ch>
2025-02-08 16:01:51 +0100

    env example and git ignore

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from website import Website
+from modelOpenAi import ModelOpenAI
+
+openAi = ModelOpenAI()
+openAi.initialize_model()
 
 website = Website('http://creativewin.net')
-print(website.get_links())
+ 
---
+++ modelOpenAi.py
@@ -0,0 +1,28 @@
+
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+DEFAULT_MODEL = "gpt-4o-mini"
+
+class ModelOpenAI():
+  def __init__(self, model=DEFAULT_MODEL):
+    load_dotenv(override=True)
+    self.api_key = os.getenv('OPENAI_API_KEY')
+    if not self.api_key:
+      print("No API key was found - please check your .env file!")
+    else:
+      print("OpenAI API key found and looks good so far! \n\n")
+      self.model = model
+      self.initialize_model()
+
+  def initialize_model(self):
+    self.openai = OpenAI(api_key=self.api_key)
+
+  def chat(self, prompt):
+    response = self.openai.chat.completions.create(
+      model = self.model,
+      messages = prompt
+    ) 
+    return response.choices[0].message.content
